57b50d6929d40f2eee0e0d1d16b654b62846484c
Ziyi Yang <ziyiyang8@gmail.com>
2014-02-05 16:15:17 -0800

    initial gem setup

---
+++ spec/features/account_creation_feature_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'account creation' do
+	it 'allows user to create account' do
+		visit root_path
+		click_link 'Create Account'
+
+		fill_in 'Name', with: 'Ziyi'
+		fill_in 'Email', with: 'ziyiyang@gmail.com'
+		fill_in 'Password', with: 'password'
+		fill_in 'Password Confirmation', with: 'password'
+		fill_in 'Subdomain', with: 'test_subdomain'
+		click_button 'Create Account'
+
+		expect(page).to have_content('Signed up successfully')
+	end
+end
